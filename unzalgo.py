a1be82933091f8aa86e86ef63e4d56e8f4442c5b
Kevin <75402062+Kev-in123@users.noreply.github.com>
2021-05-23 15:27:14 -0400

    Update unzalgo.py

--- unzalgo.py
+++ unzalgo.py
@@ -1,8 +1,5 @@
 import re
 text="H̙ͤ́e͍̤̠l̩͋͞l̸̈́̚o" #change this to the zalgo text
-unzalgo=lambda word:re.sub("(?i)([aeiouy]̈)|[̀-ͯ҉]","\\1",word) #note that this method can't filter 𝔱𝔢𝔵𝔱 𝔩𝔦𝔨𝔢 𝔱𝔥𝔦𝔰 
-print(unzalgo(text))
-
-#line 3 can be replaced with:
+text = unicodedata.normalize("NFKC", text)
 unzalgo=lambda word:re.sub("[^a-zA-Z0-9 \n.]", "", text)
-#but if 𝔱𝔢𝔵𝔱 𝔩𝔦𝔨𝔢 𝔱𝔥𝔦𝔰 is the string, there will be no output
+print(unzalgo(text))
