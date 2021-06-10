746935e2cd62bb934d350e3f4fbbebb6000ae184
Kevin <75402062+Kev-in123@users.noreply.github.com>
2021-06-09 21:54:50 -0400

    Update typewriter.py

--- typewriter.py
+++ typewriter.py
@@ -1,5 +1,5 @@
-string='message' #change this to the string you want
+message='message' #change this to the string you want
 def typewriter(msg):  
   for char in msg:  
     print(char,end="",flush=True)
-typewriter(string) 
+typewriter(message) 
