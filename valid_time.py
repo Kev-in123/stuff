4a7a3743ac127bb71a530d86ba873fc14348f8e7
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-06 14:56:14 -0400

    Update valid_time.py

--- valid_time.py
+++ valid_time.py
@@ -4,7 +4,7 @@ import re
 time_regex=re.compile(r"(0[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] ([AP]M)")
 
 def valid_time(time: str):
-    return bool(time_regex.findall(time))
+    return bool(time_regex.match(time))
 
 time=input(">")
 print(valid_time(time)) 
