a73a9bc08fb8f36df17c8b0b3fd9a4ca22be1d69
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-06 13:24:45 -0400

    Update and rename validtime.py to valid_time.py

--- valid_time.py
+++ valid_time.py
@@ -6,4 +6,5 @@ time_regex=re.compile(r"(0[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] ([AP]M)")
 def valid_time(time: str):
     return bool(time_regex.findall(time))
 
-print(valid_time("10:10:10 PM"))
+time=input(">")
+print(valid_time(time)) 
