9b2a6a06864d37f9cf537c061d6d2eedeee88198
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-06 13:23:23 -0400

    Update validtime.py

--- validtime.py
+++ validtime.py
@@ -1,4 +1,4 @@
-#must use `hh:mm:ss xx` format example: `10:20:36 PM`
+#must use `hh:mm:ss xx` format. example: `10:20:36 PM`
 import re
 
 time_regex=re.compile(r"(0[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] ([AP]M)")
