2b6fcfae4ddd632c08b21d7bb42f78dff0e4b77a
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-06 14:57:07 -0400

    Update unzalgo.py

--- unzalgo.py
+++ unzalgo.py
@@ -1,6 +1,6 @@
 import re
 import unicodedata
-text="H̙ͤ́e͍̤̠l̩͋͞l̸̈́̚o" #change this to the zalgo text or alternatively use input
+text=input(">")  #alternatively you could pass in a string
 text = unicodedata.normalize("NFKC", text)
 unzalgo=lambda word:re.sub("[^a-zA-Z0-9 \n.]", "", text)
 print(unzalgo(text))
