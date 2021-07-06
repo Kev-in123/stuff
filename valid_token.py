eb2ddfeeb8d5fc295ba38f629297cfb4eb3909b3
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-06 13:25:07 -0400

    Create valid_token.py

---
+++ valid_token.py
@@ -0,0 +1,9 @@
+import re
+
+token_regex = re.compile(r"[a-zA-Z0-9_-]{23,28}\.[a-zA-Z0-9_-]{6,7}\.[a-zA-Z0-9_-]{27}")
+
+def valid_token(token: str):
+    return bool(token_regex.findall(token))
+  
+token=input(">")
+print(valid_token(token)) 
