fb7c38a1c0a9c0519e510fe634cadcf09bf6438b
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-14 13:41:12 -0400

    Update typewriter.py

--- typewriter.py
+++ typewriter.py
@@ -1,6 +1,6 @@
 """enter some text and have it printed in a typewriter style"""
 message = input(">") #alternatively you could pass in a string
 def typewriter(msg):  
-  for char in msg:  
-    print(char,end="",flush=True)
+    for char in msg:  
+        print(char ,end="", flush=True)
 typewriter(message) 
