f852f8c7ee176fbc9ce7833ae3b14f01adcce76c
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-14 13:41:40 -0400

    Update typewriter.py

--- typewriter.py
+++ typewriter.py
@@ -2,5 +2,5 @@
 message = input(">") #alternatively you could pass in a string
 def typewriter(msg):  
     for char in msg:  
-        print(char ,end="", flush=True)
+        print(char, end="", flush=True)
 typewriter(message) 
