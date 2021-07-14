f96a2d48dc141b9d8318f920f8ab68377d30301a
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-14 13:42:49 -0400

    Update valid_token.py

--- valid_token.py
+++ valid_token.py
@@ -4,7 +4,7 @@ import re
 token_regex = re.compile(r"[a-zA-Z0-9_-]{23,28}\.[a-zA-Z0-9_-]{6,7}\.[a-zA-Z0-9_-]{27}")
 
 def valid_token(token: str):
-    return bool(token_regex.match(token))
+        return bool(token_regex.match(token))
   
-token=input(">") #alternatively you could pass in a string
+token = input(">") #alternatively you could pass in a string
 print(valid_token(token)) 
