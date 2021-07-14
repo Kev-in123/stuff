284d07308ef4cb1180de0f8132bb484656a78c33
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-14 13:41:56 -0400

    Update valid_time.py

--- valid_time.py
+++ valid_time.py
@@ -1,10 +1,10 @@
 """check if a time is valid with the `hh:mm:ss xx` format"""
 import re
 
-time_regex=re.compile(r"(0[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] ([AP]M)")
+time_regex = re.compile(r"(0[1-9]|1[0-2]):[0-5][0-9]:[0-5][0-9] ([AP]M)")
 
 def valid_time(time: str):
     return bool(time_regex.match(time))
 
-time=input(">") #alternatively you could pass in a string
+time = input(">") #alternatively you could pass in a string
 print(valid_time(time)) 
