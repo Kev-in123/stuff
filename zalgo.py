de5a1fcea7472483f1ce26083e5628ca81c1333e
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-14 13:43:47 -0400

    Update zalgo.py

--- zalgo.py
+++ zalgo.py
@@ -6,16 +6,16 @@ message = input(">") #alternatively you could pass in a string
 
 def zalgo(string):
   for char in string:
-    if char ==" ":
-      string+=" "
+    if char == " ":
+      string += " "
     else:
       intensity=3
       for i in range(intensity):
-        zalgo_char=random.choice(range(768, 879))
-        string+=chr(zalgo_char)
-      string+=char
-  half=int(len(string)/4)
-  word=string[half:]
+        zalgo_char = random.choice(range(768, 879))
+        string += chr(zalgo_char)
+      string += char
+  half = int(len(string)/4)
+  word = string[half:]
   return word
 
 print(zalgo(string))
