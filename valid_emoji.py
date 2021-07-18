2e4235295fae19462e4c34a5c1ef6e2761ecba91
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-18 11:21:47 -0400

    Create valid_emoji.py

---
+++ valid_emoji.py
@@ -0,0 +1,10 @@
+"""validates a discord emoji"""
+import re
+
+emoji_regex = re.compile(r"<a*:[a-zA-Z]+:[0-9]+>")
+
+def valid_emoji(emoji: str):
+    return bool(emoji_regex.match(emoji))
+  
+emoji = input(">") #alternatively you could pass in a string
+print(valid_emoji(emoji)) 
