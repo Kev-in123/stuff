7ff54b14547febae97be2007fd83608aa8beb625
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-18 22:30:21 -0400

    Update valid_emoji.py

--- valid_emoji.py
+++ valid_emoji.py
@@ -6,5 +6,5 @@ emoji_regex = re.compile(r"<a*:[a-zA-Z]+:[0-9]{18}>")
 def valid_emoji(emoji: str):
     return bool(emoji_regex.match(emoji))
   
-emoji = input(">") #alternatively you could pass in a string
+emoji = input() #alternatively you could pass in a string
 print(valid_emoji(emoji)) 
