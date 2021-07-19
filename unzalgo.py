7bdbf30a546c742cef3fe0db36efd41668d8bc79
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-07-18 22:29:48 -0400

    Update unzalgo.py

--- unzalgo.py
+++ unzalgo.py
@@ -4,5 +4,7 @@ import re
 import unicodedata
 text = input()  #alternatively you could pass in a string
 text = unicodedata.normalize("NFKC", text)
-unzalgo = lambda text: re.sub("[^a-zA-Z0-9 \n.]", "", text)
+def unzalgo (text):
+  return re.sub("[^a-zA-Z0-9 \n.]", "", text)
+
 print(unzalgo(text))
