b8cc63ed38cb349e58e612cd902194c773a4b7ce
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-09 15:40:15 -0400

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,8 @@
+def factorial(num): #create a function
+  fact = 1 #initialize factorial as 1
+  for i in range(1,num+1): #loop to the number
+    fact *=  i #keep multiplying untill the number is reached
+  print (f"The factorial of {num} is: {fact}") #print the result
+
+num = int(input("Enter a number: ")) #alternatively you could pass in an integer
+factorial(num) #call the function
