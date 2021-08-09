2e737709fb45bc464241c09cf1a349e1c861e00b
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-09 15:53:15 -0400

    Update rhyme.py

--- regex/rhyme.py
+++ regex/rhyme.py
@@ -1,11 +1,10 @@
-import re
+import re #import regex
+pattern = "(\w*oor|\w*ore)" #pattern to serach for
+rhymingWords = [] #the rhyming words
 
-pattern = "(\w*oor|\w*ore)"
-rhymingWords = []
+with open("TheRaven.txt", "r") as f: #open the file
+  for line in f: #read the lines
+    rhymingWords.extend(re.findall(pattern, line, re.IGNORECASE)) #add the rhyming words
 
-with open("TheRaven.txt", "r") as fi:
-  for line in fi:
-    rhymingWords.extend(re.findall(pattern, line, re.IGNORECASE))
-
-print(rhymingWords)
-print(len(rhymingWords))
+print(rhymingWords) #print the rhyming words
+print(len(rhymingWords)) #print the amount of rhyming words
