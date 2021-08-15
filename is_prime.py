c37f99a6b9952118f4cefa4c50fbacb8038f2465
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-14 23:27:07 -0400

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,6 +1,6 @@
 """checks if a number is prime"""
 def isPrime(num):
-    return 2 in [num, 2 ** num % num]
+    return 2 in (num, 2 ** num % num)
 
 num = int(input()) #alternatively you could pass in a number    
 print(isPrime(num))
