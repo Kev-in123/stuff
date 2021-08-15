ea0226cd4737181fc6b685ab6167933ae0616cc6
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-08-14 23:25:55 -0400

    change list to tuple
    change list to tuple, faster cuz they wont be modified

--- hanoi.py
+++ hanoi.py
@@ -2,22 +2,21 @@
 
 def TowerOfHanoi(n, fromRod, toRod, spareRod): #create a function
     if n == 1: #avoid stack overflowing
-        print(f'Move disk 1 from the {fromRod} rod to the {toRod} rod') #move the last disk
+        print(f"Move disk 1 from the {fromRod} rod to the {toRod} rod") #move the last disk
         return #exit the function
     TowerOfHanoi(n-1, fromRod, spareRod, toRod) #call the function with a rod moved
-    print(f'Move disk {n} from the {fromRod} rod to the {toRod} rod') #move the disk
+    print(f"Move disk {n} from the {fromRod} rod to the {toRod} rod") #move the disk
     TowerOfHanoi(n-1, spareRod, toRod, fromRod) #call the function with a rod moved
 
-rods = ["left","middle","right"] #initialize the possible rods
+rods = ("left","middle","right") #initialize the possible rods
 used_rods = [] #initialize the used rods
 
-numDisks = int(input('Enter a number of disks: ')) #prompt for a number of disks; alternatively you could pass in an integer
-fromRod = str(input('Enter the original rod: ')) #prompt for the original rod; alternatively you could pass in a string
-toRod = str(input('Enter the rod you are moving the disks to: ')) #prompt for the target rod; alternatively you could pass in a string
+numDisks = int(input("Enter a number of disks: ")) #prompt for a number of disks; alternatively you could pass in an integer
+fromRod = str(input("Enter the original rod: ")) #prompt for the original rod; alternatively you could pass in a string
+toRod = str(input("Enter the rod you are moving the disks to: ")) #prompt for the target rod; alternatively you could pass in a string
 
 used_rods.append(fromRod.lower()) #add the used rods to the list
 used_rods.append(toRod.lower())
-spare_rod = list(set(rods) - set(used_rods))[0] #find the remaining rod
+spare_rod = tuple(set(rods) - set(used_rods))[0] #find the remaining rod
 
 TowerOfHanoi(numDisks, fromRod, toRod, spare_rod) #call the function
-
