f88ee24a08d450fb13c3de67de27596ef33ac89f
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-09-06 22:05:41 -0400

    Code formatting and added user input

--- fibonacci.java
+++ fibonacci.java
@@ -1,20 +1,25 @@
+import java.util.Scanner;
+
 public class fibonacci {
 	public static void main (String[] args) {
-	  System.out.println(calculate(15));
+    Scanner s = new Scanner(System.in);
+    int n = s.nextInt();
+    System.out.println(calculate(n));
+    s.close();
 	}
   
 	public static long calculate(int n) {
-	  if (n == 2 || n == 1) {
-		return 1;
-	  }
-	  long r1 = 1;
-	  long r2 = 1;
-	  long r = 0;
-	  for (int i = 0; i < n - 2; ++i) {
-		r = r1 + r2;
-		r2 = r1;
-		r1 = r;
-	  }
-	  return r;
+      if (n == 2 || n == 1) {
+        return 1;
+      }
+      long r1 = 1;
+      long r2 = 1;
+      long r = 0;
+      for (int i = 0; i < n - 2; ++i) {
+        r = r1 + r2;
+        r2 = r1;
+        r1 = r;
+      }
+      return r;
 	}
 }
