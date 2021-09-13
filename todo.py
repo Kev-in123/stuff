bb96d12d61483126268fe91f00e76350af70e478
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-09-12 21:46:57 -0400

    Create todo.py

---
+++ todo.py
@@ -0,0 +1,32 @@
+class TODO:
+
+    def __init__(self):
+      self.todo = []
+
+    def _add(self, item):
+      self.todo.append(item)
+
+    def _remove(self, item: str):
+      self.todo.pop(self.todo.index(item))
+
+    def _list(self, item = None):
+      todo = self.todo
+      if not item:
+        print("\tTODO\n-----------------------")
+        for i in range(len(todo)):
+          print(f"{i+1}. {todo[i]}")
+        print()
+        
+      else:
+        print(self.todo[item-1])
+
+    def _switch(self, item1, item2):
+      self.todo[item1-1], self.todo[item2-1] = self.todo[item2-1], self.todo[item1-1]
+
+t = TODO()
+t._add("thing 1")
+t._add("thing 2")
+t._list()
+
+t._switch(1, 2)
+t._list()
