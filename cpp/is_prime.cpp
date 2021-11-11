f3f7b6b88e9c42559dd28bf1aeee7f0080abfc70
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-11 15:51:50 -0500

    Create is_prime.cpp

---
+++ cpp/is_prime.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+bool isPrime(int n) {
+    if (n < 2) { //check if the number is less than 2
+        return false;
+    }
+    else if (n == 2) { //if the number is 2
+        return true;
+    } //if the number is prime and isn't 2 the check below should be true
+    return (int) pow(2, n) % n == 2;
+}
+int main() {
+	int n;
+	std::cin >> n;
+    std::cout << isPrime(n);
+}
