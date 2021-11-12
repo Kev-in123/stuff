8e73420a54c1430e194d780d6ae48e9591b44c2b
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-12 18:02:04 -0500

    Create factorial.cpp

---
+++ cpp/factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+void factorial(int n) {
+    int fact = 1;
+    for (int i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    std::cout << "The factorial of " << n << " is: " << fact;
+}
+
+int main() {
+    factorial(5);
+}
