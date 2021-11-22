2f273912c3da42295d1862365a120649d25a76d8
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-21 23:08:03 -0500

    Create what.cpp

---
+++ cpp/what.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+    std::string e = "no.";
+    std::string& ee = e; //refrence var
+    std::string* eee = &e; //pointer var
+    std::cout << e << "\n";
+    std::cout << ee << "\n";
+    std::cout << eee << "\n";
+    e = "yes.";
+    ee = "idk.";
+    std::cout << e << "\n";
+    std::cout << ee << "\n";
+    std::cout << eee << "\n";
+}
