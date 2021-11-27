ce312f1bcbf8f708da0ae4af5db0939048c183b7
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-27 13:40:54 -0500

    Update is_prime.py

--- py/is_prime.py
+++ py/is_prime.py
@@ -4,7 +4,7 @@ def isPrime(num):
       return False
     elif (num == 2):
       return True
-    return 2 ** num % num == 2 # at least one of the numbers will be 2
+    return 2 ** num % num == 2 # this will return true for most cases
 
 num = int(input()) #alternatively you could pass in a number    
 print(isPrime(num)) #print
