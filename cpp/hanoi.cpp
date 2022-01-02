a2c2727114b97286e53df1bac23ac31ae8297c1d
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-01-02 08:41:16 -0500

    Update hanoi.cpp

--- cpp/hanoi.cpp
+++ cpp/hanoi.cpp
@@ -4,12 +4,12 @@
 
 void TowerOfHanoi(int n, string fromRod, string toRod, string spareRod) {
     if (n == 1) {
-        std::cout << "Move disk 1 from rod " + fromRod + " to rod " + toRod + "\n"; // move the last disk
+        std::cout << "Move disk 1 from rod " + fromRod + " to rod " + toRod + "\n"; // display the step
     }
     else {
-        TowerOfHanoi(n - 1, fromRod, spareRod, toRod); // call the function with a disk moved
-        std::cout << "Move disk " << n << " from rod " + fromRod + " to rod " + toRod + "\n"; //move the disk
-        TowerOfHanoi(n - 1, spareRod, toRod, fromRod); // call the function with a disk moved
+        TowerOfHanoi(n - 1, fromRod, spareRod, toRod); // move the disk
+        std::cout << "Move disk " << n << " from rod " + fromRod + " to rod " + toRod + "\n"; // display the step
+        TowerOfHanoi(n - 1, spareRod, toRod, fromRod); // move the disk
     }
 }
 
