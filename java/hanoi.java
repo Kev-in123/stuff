76f54987295cedb94c3f5e9d667dd4c51e66c3c6
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-01-02 08:41:37 -0500

    Update hanoi.java

--- java/hanoi.java
+++ java/hanoi.java
@@ -4,11 +4,11 @@ public class hanoi {
   }
   public static void TowerOfHanoi(int n, String fromRod, String toRod, String spareRod) {
     if (n == 1) {
-      System.out.println("Move disk 1 from rod " + fromRod + " to rod " + toRod); // move the last disk
+      System.out.println("Move disk 1 from rod " + fromRod + " to rod " + toRod); /// display the step
     } else {
-      TowerOfHanoi(n - 1, fromRod, spareRod, toRod); // call the function with a disk moved
-      System.out.println("Move disk " + n + " from rod " + fromRod + " to rod " + toRod); //move the disk
-      TowerOfHanoi(n - 1, spareRod, toRod, fromRod); // call the function with a disk moved
+      TowerOfHanoi(n - 1, fromRod, spareRod, toRod); // move the disk
+      System.out.println("Move disk " + n + " from rod " + fromRod + " to rod " + toRod); // display the step
+      TowerOfHanoi(n - 1, spareRod, toRod, fromRod); // move the disk
     }
   }
 }
