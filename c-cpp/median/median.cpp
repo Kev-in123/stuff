b209861cfa9a3686f62bb6de828aef3c6283de5d
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-06-16 17:40:59 -0400

    Update median.cpp

--- c-cpp/median/median.cpp
+++ c-cpp/median/median.cpp
@@ -2,20 +2,20 @@
 #include <list>
 
 void Median::addNum(int n) {
-    nums.push_back(n);
+  nums.push_back(n);
 }
 
 double Median::findMedian() {
-    nums.sort();
-    std::list<int>::iterator index = nums.begin();
-    int length = nums.size();
-    int middle = length / 2;
-    std::advance(index, middle);
-    int val = *index;
-    if (length % 2 != 0) {
-        return val;
-    }
-    std::advance(index, -1);
-    int val2 = *index;
-    return (double) (val + val2) / 2;
+  nums.sort();
+  std::list<int>::iterator index = nums.begin();
+  int length = nums.size();
+  int middle = length / 2;
+  std::advance(index, middle);
+  int val = *index;
+  if (length % 2 != 0) {
+      return val;
+  }
+  std::advance(index, -1);
+  int val2 = *index;
+  return (double) (val + val2) / 2;
 }
