7bc6656755fed66b60a72c3b90ecd8dc5dacbf31
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-06-16 17:40:24 -0400

    Update main.cpp

--- c-cpp/median/main.cpp
+++ c-cpp/median/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include "median.h"
 
-int main()
-{
-	Median m;
-	m.addNum(1);
-	m.addNum(10);
-	std::cout << m.findMedian();
+int main() {
+  Median m;
+  m.addNum(1);
+  m.addNum(10);
+  std::cout << m.findMedian();
 }
